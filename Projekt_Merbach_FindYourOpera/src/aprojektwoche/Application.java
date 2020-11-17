615de558a7894c7259c7abb32ca30bbe9884f388
Frog on a Cello <jm(at)frog-on-a-cello.com>
2020-11-17 09:17:22 +0100

    Init

---
+++ Projekt_Merbach_FindYourOpera/src/aprojektwoche/Application.java
@@ -0,0 +1,14 @@
+package aprojektwoche;
+
+import aprojektwoche.controller.Controller;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		@SuppressWarnings("unused")
+		Controller controller = new Controller();	
+		
+	}
+	
+}
